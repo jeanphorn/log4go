bda7e20537d3dd1eb4fd5c145806791b10ba7210
walkon <465276914@qq.com>
2021-08-17 16:53:27 +0800

    Update pattlog.go
    add microsecond

--- pattlog.go
+++ pattlog.go
@@ -17,7 +17,7 @@ const (
 )
 
 type formatCacheType struct {
-	LastUpdateSeconds    int64
+	LastUpdateMicros    int64
 	shortTime, shortDate string
 	longTime, longDate   string
 }
@@ -42,19 +42,19 @@ func FormatLogRecord(format string, rec *LogRecord) string {
 		return ""
 	}
 
-	out := bytes.NewBuffer(make([]byte, 0, 64))
-	secs := rec.Created.UnixNano() / 1e9
+	out := bytes.NewBuffer(make([]byte, 0, 72))
+	micro := rec.Created.UnixNano() / 1e3
 
 	cache := *formatCache
-	if cache.LastUpdateSeconds != secs {
+	if cache.LastUpdateMicros != micro {
 		month, day, year := rec.Created.Month(), rec.Created.Day(), rec.Created.Year()
 		hour, minute, second := rec.Created.Hour(), rec.Created.Minute(), rec.Created.Second()
 		zone, _ := rec.Created.Zone()
 		updated := &formatCacheType{
-			LastUpdateSeconds: secs,
+			LastUpdateMicros: micro,
 			shortTime:         fmt.Sprintf("%02d:%02d", hour, minute),
 			shortDate:         fmt.Sprintf("%02d/%02d/%02d", day, month, year%100),
-			longTime:          fmt.Sprintf("%02d:%02d:%02d %s", hour, minute, second, zone),
+			longTime:          fmt.Sprintf("%02d:%02d:%02d.%06d %s", hour, minute, second, micro%1e6, zone),
 			longDate:          fmt.Sprintf("%04d/%02d/%02d", year, month, day),
 		}
 		cache = *updated
